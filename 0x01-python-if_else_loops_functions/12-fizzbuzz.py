13c4be85037e206e609a2b7230c5c7a79f12232c
BugFixDotExe <bugfix.exe@gmail.com>
2024-03-12 09:37:43 -0700

    Alhamdullahi fizzbuzz testing time

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def fizzbuzz():
     for n in range(1, 101):
+        if n % 3 == 0 and n % 5 == 0:
+            print("{:s}".format("FizzBuzz"), end=" ")
+            continue
         if n % 3 == 0:
             print("{:s}".format("Fizz"), end=" ")
         elif n % 5 == 0:
             print("{:s}".format("Buzz"), end=" ")
-        elif n % 3 == 0 and n % 5 == 0:
-            print("{:s}".format("FizzBuzz"), end=" ")
         else:
             print("{:d}".format(n), end=" ")
