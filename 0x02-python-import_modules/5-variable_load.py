4f6d0c6af1f582d1fc419a4e3f54b4da743e211c
BugFixDotExe <bugfix.exe@gmail.com>
2024-03-14 05:47:44 -0700

    debugging

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import varaible_load_5 as va
+    import variable_load_5  as va
     var = va.a
-    print("{:d}".format(var)
+    print("{:d}".format(var))
