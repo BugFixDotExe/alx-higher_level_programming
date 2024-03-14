9d7980e233f51595aaec09607817625f5c61c6db
BugFixDotExe <bugfix.exe@gmail.com>
2024-03-14 05:03:07 -0700

    debugging

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import calculator_1 as cal
+import calculator_1
 if __name__ == "__main__":
     a = 10
     b = 5
-    print("{:d} + {:d} = {:d}".format(a, b, cal.add(a, b)))
-    print("{:d} - {:d} = {:d}".format(a, b, cal.sub(a, b)))
-    print("{:d} * {:d} = {:d}".format(a, b, cal.mul(a, b)))
-    print("{:d} / {:d} = {:d}".format(a, b, cal.div(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, calculator_1.add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, calculator_1.sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, calculator_1.mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, calculator_1.div(a, b)))
