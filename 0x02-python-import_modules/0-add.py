3b7e11905a490bf8244159a06ec44c987148037f
BugFixDotExe <bugfix.exe@gmail.com>
2024-03-14 05:10:44 -0700

    debugging

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,4 +3,4 @@ if __name__ == "__main__":
     import add_0 as a
     a = 1
     b = 2
-    print("{:s} + {:s} = {:s}".format(str(a), str(b), str(a.add(a, b))))
+    print("{} + {} = {}".format(a, b, a.add(a, b)))
