6be6e180b98cfbcdf3d14540cc6ec81097192ec1
BugFixDotExe <bugfix.exe@gmail.com>
2024-03-14 04:56:44 -0700

    bugfix for not printing with string format

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     a = 1
     b = 2
     sum_of_int = add_0.add(a, b)
-    print("{} + {} = {}".format(a, b, sum_of_int))
+    print("{:s} + {:s} = {:s}".format(a, b, sum_of_int))
