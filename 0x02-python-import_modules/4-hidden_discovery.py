c7e888a61df4d6cffdda251b21939de576ed350a
BugFixDotExe <bugfix.exe@gmail.com>
2024-03-14 06:29:38 -0700

    insha Allah solution to 4-hidden

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    dir_list = dir(hidden_4)
+    for i in range(len(dir_list)):
+        if dir_list[i][0] == '_':
+            continue
+        print("{:s}".format(dir_list[i]))
