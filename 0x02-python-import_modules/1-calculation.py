5e771b69a8801edd907bc1a4728749eaba73f81a
BugFixDotExe <bugfix.exe@gmail.com>
2024-03-14 05:04:44 -0700

    left the import out of the if its imported check

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import calculator_1
 if __name__ == "__main__":
+    import calculator_1
     a = 10
     b = 5
     print("{:d} + {:d} = {:d}".format(a, b, calculator_1.add(a, b)))
