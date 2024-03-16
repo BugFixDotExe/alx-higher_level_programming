902ab646d70b5e099b92b6c90e7954f3fc79b069
BugFixDotExe <bugfix.exe@gmail.com>
2024-03-16 04:46:51 -0700

    Alhamdullahi and with the permission of Allah, a solution the negate the uppercase C and c problem

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for c in my_string:
-        if c == 'c' or c == 'C':
-            new_str = my_string.replace(c, "")
-    return (new_str)
+    sub_str = []
+    for i in range(len(my_string)):
+        if my_string[i] == "c" or my_string[i] == "C":
+            continue
+        else:
+            sub_str.append(my_string[i])
+    _str = ''.join(str(char) for char in sub_str)
+    return (_str)
