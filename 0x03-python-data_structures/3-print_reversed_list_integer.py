67604058c5d7398daf2be7533549f03b9c583630
BugFixDotExe <bugfix.exe@gmail.com>
2024-03-16 03:13:32 -0700

    handled an edge case where the list had only one value in itq

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    i = 0
-    len_of_list = len(my_list) - 1
-    while len_of_list >= i:
-        print("{:d}".format(my_list[len_of_list]))
-        len_of_list -= 1
+    if len(my_list) == 1:
+        print("{:d}".format(my_list[0]))
+    else:
+        len_of_list = len(my_list) - 1
+        while len_of_list  >= 0:
+            print("{:d}".format(my_list[len_of_list]))
+            len_of_list -= 1
