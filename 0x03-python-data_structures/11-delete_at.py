8b3cba7316ac36d1a43ab0b69aec133676ce00f2
BugFixDotExe <bugfix.exe@gmail.com>
2024-03-16 05:36:19 -0700

    solution to to delete at Alhamdullahi all prasie it to Allah

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
+    ret_list = []
     len_of_list = len(my_list) - 1
     if idx > len_of_list or idx < 0:
         return (my_list)
     for i in range(len(my_list)):
         if i == idx:
-            my_list[idx] = my_list[idx+1]
-        my_list[i] = my_list[i+1]
-
+            del my_list[i]
     return (my_list)
