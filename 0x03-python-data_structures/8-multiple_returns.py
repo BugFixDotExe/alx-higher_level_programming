699f7bf69653d3ce9382e2f160e2b78a167f9925
BugFixDotExe <bugfix.exe@gmail.com>
2024-03-18 01:12:35 -0700

    Alhamdullahi, debigging of 8th problem

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    str_list = []
     len_of_sen = len(sentence)
+    for val in sentence:
+        str_list.append(val)
     if len_of_sen == 0:
-        sentence[0] = None
-    multi_ret = len_of_sen, sentence[0]
+        str_list.append(None)
+    multi_ret = len_of_sen, str_list[0]
     return (multi_ret)
