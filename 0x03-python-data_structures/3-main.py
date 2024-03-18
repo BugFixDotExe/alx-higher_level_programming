94e10c06d38652cf9d78bf46f1a4d1397751ea62
BugFixDotExe <bugfix.exe@gmail.com>
2024-03-18 01:16:47 -0700

    Alhamdullahi, debugging the revwqerse the list

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
-my_list = []
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print_reversed_list_integer(my_list)
