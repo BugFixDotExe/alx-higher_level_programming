5bce3482825fa13addb546772ee2455cd1b1a612
bugfix.exe <bugfix.exe@gmai.com>
2024-03-19 01:02:04 -0700

    debugging the 3-print_reveres problem

--- 0x03-python-data_structures/3-main.py
+++ 0x03-python-data_structures/3-main.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 print_reversed_list_integer = __import__('3-print_reversed_list_integer').print_reversed_list_integer
-my_list = []
+my_list = None
 print_reversed_list_integer(my_list)
--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if my_list == None:
+        return
     if len(my_list) == 0:
         return (0)
     elif len(my_list) == 1:
