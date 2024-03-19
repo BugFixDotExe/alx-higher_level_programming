2062eda6bc3de6b54222e9f327d4b98eb6bae60d
bugfix.exe <bugfix.exe@gmai.com>
2024-03-19 01:41:11 -0700

    solution to the tuple sum

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -24,7 +24,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
         if len_b > 2:
             c, d = tuple_b[0], tuple_b[1]
 
-    elif len_a == 2 or len_b == 2:
+    if len_a == 2 or len_b == 2:
         if len_a == 2:
             a, b = tuple_a[0], tuple_a[1]
         if len_b == 2:
