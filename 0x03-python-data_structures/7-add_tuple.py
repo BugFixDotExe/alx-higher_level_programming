eb79afea767ecb6fb4eff5d22bdfcfad4cda90ba
bugfix.exe <bugfix.exe@gmai.com>
2024-03-19 01:20:15 -0700

    Alhamdullahi, debugigng the add tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,8 +4,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
     b = 0
     c = 0
     d = 0
+
     len_a = len(tuple_a)
     len_b = len(tuple_b)
+
     if len_a < 2 or len_b < 2:
         if len_a == 0:
             a, b = 0, 0
@@ -16,12 +18,14 @@ def add_tuple(tuple_a=(), tuple_b=()):
             a, b = tuple_a[0], 0
         elif len_b == 1:
             c, d = tuple_b[0], 0
+
     if len_a > 2 or len_b > 2:
         if len_a > 2:
             a, b = tuple_a[0], tuple_a[1]
-        elif len_b > 2:
+        if len_b > 2:
             c, d = tuple_b[0], tuple_b[1]
-    if len_a == 2 or len_b == 2:
+
+    elif len_a == 2 or len_b == 2:
         if len_a == 2:
             a, b = tuple_a[0], tuple_a[1]
         if len_b == 2:
