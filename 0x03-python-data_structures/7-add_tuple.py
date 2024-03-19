a50baf4995d5b4bd0518fa19338afa053366dc7f
bugfix.exe <bugfix.exe@gmai.com>
2024-03-19 01:34:34 -0700

    solution to add tuple Alhamdullahi

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,16 +7,15 @@ def add_tuple(tuple_a=(), tuple_b=()):
 
     len_a = len(tuple_a)
     len_b = len(tuple_b)
-
     if len_a < 2 or len_b < 2:
         if len_a == 0:
             a, b = 0, 0
-        elif len_b == 0:
+        if len_b == 0:
             c, d = 0, 0
 
         if len_a == 1:
             a, b = tuple_a[0], 0
-        elif len_b == 1:
+        if len_b == 1:
             c, d = tuple_b[0], 0
 
     if len_a > 2 or len_b > 2:
