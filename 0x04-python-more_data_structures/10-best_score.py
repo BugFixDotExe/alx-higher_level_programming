3c031525581f98ee5aefcb9b6ea46bdb08341270
bugfix.exe <bugfix.exe@gmai.com>
2024-03-23 05:54:59 -0700

    debugging

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,7 +2,8 @@
 def best_score(a_dictionary):
     if a_dictionary is None or len(a_dictionary) == 0:
         return
-    a_sorted_dict = dict(sorted(a_dictionary.items()))
+    a_sorted_dict = dict(sorted(a_dictionary.items(), key=lambda x: x[1]))
+    print(a_sorted_dict)
     a_sorted_dict = list(a_sorted_dict)
     end_item = len(a_sorted_dict) - 1
     return (a_sorted_dict[end_item])
--- 0x04-python-more_data_structures/10-main.py
+++ 0x04-python-more_data_structures/10-main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 best_score = __import__('10-best_score').best_score
 
-a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-best_key = best_score(a_dictionary)
+my_dict = { 'a': 4, 'b': 3, 'c': 5, 'd': 1, 'e': 2 }
+best_key = best_score(my_dict)
 print("Best score: {}".format(best_key))
 
 best_key = best_score(None)
