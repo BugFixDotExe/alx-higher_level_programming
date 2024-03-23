45f2f7be9aa88aa4bc151e18dc70b60b29fecb83
bugfix.exe <bugfix.exe@gmai.com>
2024-03-23 01:20:15 -0700

    Alhamdullahi, pycodestyle

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    if set_1 is None and set_2 != None:
+    if set_1 is None and set_2 is not None:
         return (set_2)
-    if set_2 is None and set_1 != None:
+    if set_2 is None and set_1 is not None:
         return (set_1)
     if set_1 is None and set_2 is None:
         return
