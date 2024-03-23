547dc59df079c5b3c5ee65a934a5d594e278611d
bugfix.exe <bugfix.exe@gmai.com>
2024-03-23 01:32:11 -0700

    debugging

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,5 +2,7 @@
 def update_dictionary(a_dictionary, key, value):
     if a_dictionary is None:
         return
+    if key in a_dictionary:
+        a_dictionary[key] = value
     a_dictionary[key] = value
     return (a_dictionary)
