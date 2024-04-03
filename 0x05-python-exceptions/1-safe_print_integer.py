3bf5168f2ea473a7756fdde35cf5f7887e18981e
bugfix.exe <bugfix.exe@gmai.com>
2024-04-03 07:09:23 -0700

    debugging

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,5 +4,11 @@ def safe_print_integer(value):
         if isinstance(value, int):
             print("{:d}".format(value))
             return True
+        elif isinstance(value, list):
+            print("{:d}".format(value[0]))
+            return True
+        elif isinstance(value, tuple):
+            print("{:d}".format(value[0]))
+            return True
     except TypeError:
         return False
