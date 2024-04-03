d8ee48e6a0ef5e1f032344f62c2d61fea56705a1
bugfix.exe <bugfix.exe@gmai.com>
2024-04-03 02:47:10 -0700

    debugging

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the class Square but is is really meant as an introduction
+to python class and documentation
+"""
 #!/usr/bin/python3
 class Square:
-    """
-        Basically an empty class that is supposed to define a square
-        it serves as template for what is to come
-    """
+    """Basically an empty class that is supposed to define a square"""
     pass
