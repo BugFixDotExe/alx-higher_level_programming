590e440b8b396c27d0253f8087d97e3a610b314f
bugfix.exe <bugfix.exe@gmai.com>
2024-04-03 06:36:06 -0700

    debugging

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,8 +6,10 @@ def safe_print_list(my_list=[], x=0):
             return 0
         for i in range(x):
             if my_list[i]:
+                print("{:d}".format(my_list[i]), end="")
                 i += 1
                 continue
+        print()
         return i
     except IndexError:
         return i
