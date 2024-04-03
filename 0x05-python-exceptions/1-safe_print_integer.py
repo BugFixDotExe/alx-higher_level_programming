b80ca5c8a3404a9274e7b31343d196453210183a
bugfix.exe <bugfix.exe@gmai.com>
2024-04-03 06:50:57 -0700

    debugging

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if value + "a":
-            return False
+        if isinstance(value, int):
+            print("{:d}".format(value))
+            return True
     except TypeError:
-        print("{:d}".format(value))
         return True
