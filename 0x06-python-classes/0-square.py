56984d919c6bd5fba1bb4ccacd3805690c39f69c
bugfix.exe <bugfix.exe@gmai.com>
2024-04-03 02:40:54 -0700

    debugging

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 class Square:
     """ Basically an empty class that is supposed to define a square"""
+    def square(self):
+        """
+        A method that defines a square
+        """
+        pass
