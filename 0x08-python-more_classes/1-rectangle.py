586e207aad5baca5249370eb7332daca2ce7ca21
bugfix.exe <bugfix.exe@gmai.com>
2024-04-08 05:05:26 -0700

    solution to 1, testing time

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""This is a modue called rectangle that aims to define a rectangle"""
+
+
+class Rectangle:
+    """A class that defines a Rectangle"""
+    def __init__(self, width=0, height=0):
+        """
+        The __init__ method when an instance of the class is made
+        Args:
+            width (int): the first parameter of type integer
+        """
+
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
