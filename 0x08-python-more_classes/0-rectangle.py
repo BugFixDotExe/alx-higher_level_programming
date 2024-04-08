3c164e3b139cabbe9e461d275ed9992d92e4d6fc
bugfix.exe <bugfix.exe@gmai.com>
2024-04-08 04:40:48 -0700

    debugging

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+"""This is a module with the class Rectangle, that defines
+    the class Rectangle
+"""
 
 
 class Rectangle:
     """This is an empty class called Rectangle that defines a rectangle"""
+    pass
