dddb86811d4cccfa32dab3c6f7433528cbe4f86b
bugfix.exe <bugfix.exe@gmai.com>
2024-04-12 03:58:03 -0700

    solving and debugging for 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""A module that prints square's given the size"""
+
+
+def print_square(size):
+    """print_square function, takes size and prints a #
+        Args:
+            size (int): number of # to be printed
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    else:
+        for i in range(size):
+            for i in range(size):
+                print("#", end="")
+            print()
