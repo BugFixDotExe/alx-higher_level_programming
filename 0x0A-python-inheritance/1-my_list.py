e7c1bac57c86afe7b7815b18b93d92779ed090d1
bugfix.exe <bugfix.exe@gmai.com>
2024-04-15 05:34:42 -0700

    solved the problem 2

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""A module that prints a list"""
+
+
+class MyList(list):
+    my_list = list()
+    def print_sorted(self):
+        pass
---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""A module that returns True or False """
+
+
+def is_same_class(obj, a_class):
+    """is_same_class
+        A method that returns True if obj is
+        instance of a_class,False otherwise
+        Args:
+            obj: a python object
+            a_class: a python class
+        Returns:
+            True: if obj is instance of a_class
+            False: if obj is not instance of a_class
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
