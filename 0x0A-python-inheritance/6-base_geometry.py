b298fb2994da54acc947ab951b193b3c7d88c61f
bugfix.exe <bugfix.exe@gmai.com>
2024-04-15 06:36:02 -0700

    soltion to problem 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""A module with an empty class"""
+
+
+class BaseGeometry:
+    """BaseGeometry an empty class"""
+    def area(self):
+        """An emmpty public instance method"""
+        raise Exception("area() is not implemented")
