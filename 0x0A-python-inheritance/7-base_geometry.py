0ccb106cf2759c36b3652a8be80e14e2508131fb
bugfix.exe <bugfix.exe@gmai.com>
2024-04-15 06:41:38 -0700

    solviing problem 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A module with an empty class"""
+
+
+class BaseGeometry:
+    """BaseGeometry an empty class"""
+    def area(self):
+        """An emmpty public instance method"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """An public instance method that validates an int
+            Args:
+                name (string): a string value
+                value (int): an int value
+        """
+        if type(value) != int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
