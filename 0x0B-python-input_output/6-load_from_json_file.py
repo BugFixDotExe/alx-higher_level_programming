9fc98b9e927ce4825e71388e400580a11a7ac28b
bugfix.exe <bugfix.exe@gmai.com>
2024-04-16 07:17:16 -0700

    solution to 6-load json file to obj Alhamdullahi, testing time

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""A module that creates an Object from a JSON file"""
+import json
+
+
+def load_from_json_file(filename):
+    """load_from_json_file a function that creates an obj
+        from  a JSON file
+        Args:
+            filename (string): a filename
+        Returns:
+            Returns a python obj
+    """
+    if len(filename) == 0:
+        return None
+    with open(filename, encoding="UTF-8") as a_file:
+        return json.loads(a_file.read())
