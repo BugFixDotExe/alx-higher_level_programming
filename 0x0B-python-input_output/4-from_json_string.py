21eefd264be4ae2d3d3d6660a90a13828850315f
bugfix.exe <bugfix.exe@gmai.com>
2024-04-16 06:29:05 -0700

    soltuion to the 4-from_json_string

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""A module that returns the object representation of JSON"""
+import json
+
+
+def from_json_string(my_str):
+    """from_json_string function that returns a string
+        representation into aof a JSON reper.
+    Args:
+        my_str (string): a python string of JSON
+    Returns:
+        a JSON repersentation of my_str
+    """
+    return json.dumps(my_obj)
