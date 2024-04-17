6239fa34ca5097217819f59c86f65965b619c62e
BugFix.exe <bugfix.exe@gmail.com>
2024-04-17 10:01:05 +0100

    debugging

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
-"""a script that adds all args to a Python list, and then save them to a file"""
+"""a script that adds all args to a Python list,
+and then save them to a file"""
+import os
+import sys
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
+tokens = sys.argv[1:]
+with open("add_item.json", mode="w", encoding="UTF-8"):
+    save_to_json_file(tokens, "add_item.json")
+    load_from_json_file("add_item.json")
