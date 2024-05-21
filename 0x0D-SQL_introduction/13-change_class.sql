c1f5dd4a7a4fbea2fa1beac55319ff2fde59c421
bugfix.exe <bugfix.exe@gmai.com>
2024-05-21 10:33:57 -0700

    solution to problem 14

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,4 +1,4 @@
 -- a script that removes all records with a score <= 5 in the table
 -- of a specified  database in your MySQL server.
 DELETE FROM second_table
-	WHERE score <= 5
+	WHERE score <= 5;
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,6 @@
+-- a script that computes the score average of all records in the table
+-- of a database in your MySQL server.
+-- The result column name should be average
+-- The database name will be passed as an argument of the mysql command
+SELECT AVG(score) AS average
+	FROM second_table
