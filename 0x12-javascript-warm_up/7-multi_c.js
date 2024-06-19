3764cc525ee25d9b5ecdf9c1cde3d27bf489c507
bugfix.exe <bugfix.exe@gmail.com>
2024-06-19 15:05:08 +0100

    debugging

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,6 @@ const userInput = parseInt(process.argv[2]);
 if (isNaN(userInput)) {
   console.log('Missing number of occurrences');
 } else if (userInput <= 0) {
-  console.log();
 } else {
   for (let i = 0; i < userInput; i++) {
     console.log('C is fun');
