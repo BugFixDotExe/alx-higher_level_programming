fc5e411dc3e854c756feacf21e7bdbfc656126ac
bugfix.exe <bugfix.exe@gmail.com>
2024-06-19 15:57:42 +0100

    debugging

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+function factorial (n) {
+  if (isNaN(parseInt(n))) { return (1); } else if (n == 1) { return (1); } else {
+    return (n * factorial(n - 1));
+  }
+}
+console.log(factorial(process.argv[2]));
