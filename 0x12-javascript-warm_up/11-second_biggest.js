41886f5385d25b59ab1b07cb51c66cc2e5b95169
bugfix.exe <bugfix.exe@gmail.com>
2024-06-19 15:34:18 +0100

    debugging

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,6 +6,6 @@ if (process.argv.length <= 3) {
   for (let i = 2; i < process.argv.length; i++) {
     digits.push(process.argv[i]);
   }
+  digits.sort();
+  console.log(digits[digits.length - 2]);
 }
-digits.sort();
-console.log(digits[digits.length - 2]);
