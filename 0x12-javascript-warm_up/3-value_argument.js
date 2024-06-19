ebed3161dddbc648b9b4b6ffb9c3569b9e312d36
bugfix.exe <bugfix.exe@gmail.com>
2024-06-19 14:53:36 +0100

    debugging problem 3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,7 +3,7 @@ let index = 2;
 if (process.argv[2] === undefined) {
   console.log('No argument');
 }
-while (process.argv[index] !== undefined) {
+while (process.argv[index] != undefined) {
   console.log(process.argv[index]);
   index++;
 }
