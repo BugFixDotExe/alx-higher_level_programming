1d5dec3acd088794ee52328a7d156e6b57ff75c2
bugfix.exe <bugfix.exe@gmail.com>
2024-06-20 15:17:48 +0100

    debugging

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if ((w <= 0 || h <= 0) || (w === undefined || h === undefined)) {
-      const obj = {};
-    } else {
-      this.width = w;
-      this.height = h;
-    }
+    if ((w <= 0 || h <= 0) || (w === undefined || h === undefined)) { return; }
+    this.width = w;
+    this.height = h;
   }
 }
 module.exports = Rectangle;
