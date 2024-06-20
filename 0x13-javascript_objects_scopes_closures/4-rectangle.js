8b442f6689a2963ed7b5270fa7d7aac4b62bb38f
bugfix.exe <bugfix.exe@gmail.com>
2024-06-20 16:08:23 +0100

    solution to problem 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
     if ((w <= 0 || h <= 0) || (w === undefined || h === undefined)) {
       const newObj = {};
       return (newObj);
+    } else {
+	    this.width = w;
+	    this.height = h;
     }
   }
 
--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle.js');
-
+const Rectangle = require('./4-rectangle');
 class Square extends Rectangle {
-	constructor(size) {
-		super(size)
-	}
-	
+  constructor (size) {
+    super(size, size);
+  }
 }
 module.exports = Square;
