157514b20390476906b0c2ac08d06323c7c20639
bugfix.exe <bugfix.exe@gmail.com>
2024-06-21 15:55:04 +0100

    solution to problem 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This script is meant to fetch a database and query it for all it's items
+in ascending order
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    user, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    conn = MySQLdb.connect(host="localhost", port=3306, user=user,
+                           passwd=password, db=database, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
