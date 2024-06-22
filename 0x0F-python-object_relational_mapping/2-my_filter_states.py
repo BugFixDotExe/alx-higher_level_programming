1f765e2ba07d009cc16a4933e5cd2697ac1a43bd
bugfix.exe <bugfix.exe@gmail.com>
2024-06-22 10:37:30 +0100

    debugging problem 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=username,
                            passwd=password, db=db_name, charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT id,name\
+    cur.execute("SELECT MIN(id) as id,name\
             FROM states WHERE name LIKE '{}' GROUP BY name ORDER BY id".format(search))
     query_rows = cur.fetchall()
     for row in query_rows:
