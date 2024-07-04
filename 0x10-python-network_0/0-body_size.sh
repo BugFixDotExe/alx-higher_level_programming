4ab959565ef5517ee207fccfb2b5025680a94f56
bugfix.exe <bugfix.exe@gmai.com>
2024-07-04 05:38:27 -0700

    debugging

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # A Bash script that takes in a URL displays the size of the body of the response.
-if [[ "$#" -gt  0 ]]; then
-	curl -s --no-verbose "$1" -w "%{size_download}\n"
-else
-	exit 1
-fi
+curl -s --no-verbose "$1" -w "%{size_download}\n"
