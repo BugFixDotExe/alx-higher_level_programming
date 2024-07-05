f9320ac27160213ce511f60271e75a45a28deb04
bugfix.exe <bugfix.exe@gmai.com>
2024-07-05 04:59:51 -0700

    solution to problem 4-hbtn_status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""This is a python script that fetchs a url"""
+"""prints In the format or manner presented below"""
+
+if __name__ == "__main__":
+    import requests
+    content = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:\n\t-type: {}\n\t-content: {}".format(
+        type(content.text), content.text))
