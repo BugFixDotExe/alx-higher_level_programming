25f980249c587d7281762fca928c15a0175129b7
bugfix.exe <bugfix.exe@gmai.com>
2024-07-05 09:30:40 -0700

    solution to problem 10-my_github

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""A Python script that takes your GitHub
+credentials (username and password) and
+uses the GitHub API to display your id
+"""
+if __name__ == "__main__":
+    import requests
+    from requests.auth import HTTPBasicAuth
+    import sys
+    basic = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    content = requests.get("https://api.github.com/user", auth=basic)
+    print(content.json()["id"])
