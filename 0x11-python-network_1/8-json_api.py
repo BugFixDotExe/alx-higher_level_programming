cb5e5559c44e8c5a1f3046206af784fbc9dc2981
bugfix.exe <bugfix.exe@gmai.com>
2024-07-05 09:03:24 -0700

    solution to problem 8-json_api

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,10 +9,16 @@ if __name__ == "__main__":
     import requests
     import sys
 
-    content = requests.post(
-            "http://0.0.0.0:5000/search_user", data={'q': sys.argv[1]})
-    content = content.json()
-    if (len(content) == 0):
-        print("No result")
-    else:
-        print("[{}] {}".format(content.get("id"), content.get("name")))
+    try:
+        query = ""
+        if len(sys.argv) > 1:
+            query = sys.argv[1]
+        content = requests.post(
+                "http://0.0.0.0:5000/search_user", data={'q': query})
+        content = content.json()
+        if (len(content) == 0):
+            print("No result")
+        else:
+            print("[{}] {}".format(content.get("id"), content.get("name")))
+    except ValueError as e:
+        print("Not a valid JSON")
