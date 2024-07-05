3ee1ae7875c6b13744ef0aab94326a6924cc044b
bugfix.exe <bugfix.exe@gmai.com>
2024-07-05 04:30:14 -0700

    debugging 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,10 +8,7 @@ if __name__ == "__main__":
     with urllib.request.urlopen(
             'https://alx-intranet.hbtn.io/status') as response:
         content = response.read()
-        print("Body response:\n\t\
-                - type: {}\n\t\
-                - content: {}\n\t\
-                - utf8 content: {}".format(
+        print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".format(
                     type(content),
                     content,
                     content.decode('utf-8')))
