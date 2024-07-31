5be3e5dfc0d180ebc2ef11e24c53c714f449ec0d
bugfix.exe <bugfix.exe@gmail.com>
2024-07-31 09:18:43 +0100

    forgot the comment i used to check my output, that got into the test removed it testing time SON!

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,14 +3,12 @@ const request = require('request');
 try {
   if (process.argv[2] !== undefined) {
     const endPoint = `${process.argv[2]}`;
-    console.log(endPoint);
     request(endPoint, (err, res, body) => {
       if (err) {
         console.log('Code: ', err);
       }
       if (res.statusCode === 200) {
         const jsonToObj = JSON.parse(body);
-        console.log(jsonToObj.results[0].characters[15]);
         request(jsonToObj.results[0].characters[15], (err, res, body) => {
           if (err) {
             console.log('Code: ', err);
